851e55f962fd7bd6450d57f1a3ece8aa0349125d
Diogo Gomes <diogogomes079@gmail.com>
2018-06-04 13:05:18 -0300

    CRUD simples de Django 2.0.3

---
+++ contas/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from .models import Transacao
+from .form import TransacaoForm
+import datetime
+
+
+def home(request):
+    data={}
+
+    data['transacoes'] = ['t1', 't2', 't3', 'Diogo']
+    data['now'] = datetime.datetime.now()
+    # html = "<html><body>Is is now %s.</body></html>" %now
+
+    return render(request, 'contas/home.html', data)
+
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
+
+
+def nova_transacao(request):
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    return render(request, 'contas/form.html', {'form': form})
+
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    data['form'] = form
+    data['transacao'] = transacao
+
+    return render(request, 'contas/form.html', data)
+
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    form = TransacaoForm(request.POST or None, instance=transacao)
+    return redirect('url_listagem')
